5d88701d0403da831c04a52dea3275b0ff0f85dd
Bekarys2003IT <bekaris070603@gmail.com>
2024-04-28 00:06:57 +0600

    adding new firestore db in profile

---
+++ NewApp/AuthModels/ProfileUserRequest.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileUserRequest.swift
+//  NewApp
+//
+//  Created by Бекарыс Сандыгали on 27.04.2024.
+//
+
+import Foundation
---
+++ NewApp/ProfileModel/ProfileService.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileModel.swift
+//  NewApp
+//
+//  Created by Бекарыс Сандыгали on 27.04.2024.
+//
+
+import Foundation
+class ProfileModel {
+    public static let shared = ProfileModel()
+    private init() {}
+    
+    private func saveProfileData(with profileName:){
+        
+    }
+}
