5cc2815ac1e978b1fac9ec8e9e192495f41b2f5c
Bekarys2003IT <bekaris070603@gmail.com>
2024-05-08 19:42:54 +0500

    Adding new firestore and adjusting labels also increment of medicament

---
+++ NewApp/AuthModels/MedicineUserRequest.swift
@@ -0,0 +1,8 @@
+//
+//  MedicineUserRequest.swift
+//  NewApp
+//
+//  Created by Бекарыс Сандыгали on 06.05.2024.
+//
+
+import Foundation
---
+++ NewApp/ProfileModel/MedicineService.swift
@@ -0,0 +1,8 @@
+//
+//  MedicineService.swift
+//  NewApp
+//
+//  Created by Бекарыс Сандыгали on 06.05.2024.
+//
+
+import Foundation
