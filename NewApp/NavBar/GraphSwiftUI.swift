bef3816fe3ad57e3e1d5a69a4550612eb63f9484
Bekarys2003IT <bekaris070603@gmail.com>
2024-05-11 10:14:27 +0500

    adding graphic to my tabbar

---
+++ NewApp/NavBar/GraphSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  GraphSwiftUI.swift
+//  NewApp
+//
+//  Created by Бекарыс Сандыгали on 10.05.2024.
+//
+
+import SwiftUI
+
+struct GraphSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GraphSwiftUI()
+}
